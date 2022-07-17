af2db62210eed206e96c7637f3da35dc305e101e
John Alao <98330860+johnalao@users.noreply.github.com>
2022-07-16 20:57:28 -0400

    Add files via upload
    initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import time
+from turtle import Turtle, Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.tracer(0)
+screen.update()
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_car()
+
+    # When there is a collision between a car and the turtle
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # When the tutle reaches the finish line
+    if player.finish_line():
+        player.starting_position()
+        car_manager.increase_speed()
+        scoreboard.increase_level()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
